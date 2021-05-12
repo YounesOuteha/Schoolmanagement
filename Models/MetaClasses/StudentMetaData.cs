6c687e804dfa746ce910ceda223fbb664f5d63ea
y.outeha <younes.outeha@gmail.com>
2021-05-12 14:26:54 +0200

    Add project files.

---
+++ Models/MetaClasses/StudentMetaData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace schoolManagement.Models
+{
+    public class StudentMetaData
+    {
+
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Date of enrollment")]
+        public Nullable<System.DateTime> EnrollmentDate { get; set; }
+
+        [Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        public Nullable<System.DateTime> DateOfBirth { get; set; }
+
+    }
+
+    [MetadataType(typeof(StudentMetaData))]
+    public partial class Student { }
+}
---
+++ Startup.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+using schoolManagement.Models;
+
+[assembly: OwinStartupAttribute(typeof(schoolManagement.Startup))]
+namespace schoolManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            createRolesUsers();
+        }
+
+        public void createRolesUsers()
+        {
+            var context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            if(!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                var user = new ApplicationUser
+                {
+                    UserName = "admin",
+                    Email = "admin@schmg.com",
+                    BirthDate = System.DateTime.Now
+                };
+                var password = "password";
+                var usr = userManager.Create(user, password);
+
+                if(usr.Succeeded)
+                {
+                    var result = userManager.AddToRole(user.Id, "Admin");
+                }
+            }
+            if (!roleManager.RoleExists("Teacher"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Teacher";
+                roleManager.Create(role);
+
+            }
+            if(!roleManager.RoleExists("Supervisor"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Supervisor";
+                roleManager.Create(role);
+
+            }
+        }
+    }
+}
